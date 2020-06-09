eaa4dbdf50c61bc1ac78572dae416e70015e98de
fortress <david.la.gordt.dillie@gmail.com>
2020-06-09 16:25:40 +0200

    Add project files.

---
+++ Containervervoer/Models/Container.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Containervervoer.Models
+{
+    public class Container
+    {
+        public enum ContainerType
+        {
+            Normal,
+            Valuable,
+            Cooled
+        }
+
+        public ContainerType Type;
+        public int Weight { set; get; }
+
+        public Container(ContainerType type, int weight)
+        {
+            Weight = weight;
+            Type = type;
+        }
+
+        public override string ToString()
+        {
+            return $"Type: {Type}, Weight: {Weight}.";
+        }
+    }
+}
---
+++ Containervervoer/Models/Row.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Containervervoer.Models
+{
+    public class Row
+    {
+        public List<Container> Containers { get; } = new List<Container>();
+
+        public int Weight { get; private set; }
+        public int Height { get; }
+
+        public Row(int height)
+        {
+            Height = height;
+        }
+        private bool CheckWeight(int weight)
+        {
+            // 150000 because containers can weigh 30 and have 120 more ontop
+            if (!(Weight + weight <= 150000)) return false; 
+            else Weight = Weight + weight;
+            return true;
+        }
+
+        public bool TryToAddContainer(Container model)
+        {
+            // if weight isnt compatible, return false
+            if (!CheckWeight(model.Weight)) return false;
+
+            //if a valuable is already in the row, return false 
+            if (model.Type == Container.ContainerType.Valuable && Containers.Find(c => c.GetType() == typeof(Container.ContainerType.valuable))) return false; 
+
+            //adds the given container to the list for this row
+            Containers.Add(model);
+            return true;
+        }
+    }
+}
